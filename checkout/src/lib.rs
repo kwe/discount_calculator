8e448e486dc7b77c246148b8e28333f2ef67fe6d
kwe <kwevans@gmail.com>
2020-11-15 14:06:20 +0000

    add some structure, abstract into a library

--- checkout/src/lib.rs
+++ checkout/src/lib.rs
@@ -1,10 +1,18 @@
 
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PromotionalRules {
+    version: u32,
+    total_discount_threshold: f32,
+    total_discount_percentage: u8,
+    products: Vec<Product>,
+}
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Product {
     id: String,
-    title: String,
+    name: String,
     price: f64,
     discount_threshold: i8,
     discount_price: f64
@@ -26,10 +34,23 @@ pub fn hello() {
   println!("Hello from a library");
 }
 
+pub fn new(promotional_rules: &str){
+    unimplemented!();
+}
+
+pub fn scan(item_code: &str){
+    unimplemented!();
+}
+
+pub fn total() -> f32 {
+    4.50
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn say_hello() {
         assert_eq!(2 + 2, 4);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,50 @@
-use discounter::{self, hello};
+use checkout;
 
 
 fn main() {
-    println!("Hello, world!");
-    hello();
+
+let promotional_rules = r#"
+{
+    "version" : "v1",
+    "total_discount_threshold": 60.00,
+    "total_discount_percentage": 10,
+    "products": [
+        {
+            "id":"001",
+            "name":"Lavender heart",
+            "price":9.25,
+            "discount_threshold":2,
+            "discount_price": 8.50
+        },
+                {
+            "id":"002",
+            "name":"Personalised cufflinks",
+            "price":45.00
+        },
+                {
+            "id":"003",
+            "name":"Kids T-shirt ",
+            "price":19.95
+        }
+        ]
+}
+"#;
+
+    println!("Test data\n-----------");
+
+    let mut basket = vec!["001", "002", "003"];
+    println!("Basket: {:?}", basket);
+
+    checkout::new(promotional_rules);
+    // checkout::scan("001");
+    // checkout::scan("003");
+    let price = checkout::total();
+
+    basket = vec!["001","003","001"];
+    println!("Basket: {:#?}", basket);
+    
+    basket = vec!["001","002","001", "003"];
+    println!("Basket: {:#?}", basket);
+
+
 }
